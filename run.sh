81f152572529b4632cd8ab2f7317ec017ec3e8cf
shujuecn <yinlin2824@gmail.com>
2026-02-26 14:53:57 +0800

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 python -m tongue3d.scripts.train_autoencoder configs/autoencoder_4090_dense.yaml
 python -m tongue3d.scripts.train_image2shape configs/image2shape_4090_dense.yaml
-python -m tongue3d.scripts.infer_single runs/img2shape_4090_dense/best.pt TongueDB/images/03903.000052.png runs/predictions/03903.000052_dense.ply
+python -m tongue3d.scripts.visualize_compare TongueDB/meshes/03903.000052.obj runs/predictions/03903.000052_dense.ply runs/compare/03903_compare.png
